d29eacf1f980c8a38455783ff55fc43db530896e
Samuel Petersson <samuel.petersson@hotmail.com>
2026-03-02 14:31:21 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import loginView from '@/views/loginView.vue'
+import registerView from '@/views/registerView.vue'
+import productsView from '@/views/productsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: loginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: registerView,
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: productsView,
+    },
+  ],
+})
+
+export default router
